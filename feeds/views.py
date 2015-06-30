357818132d8e507560a1050e7cb23a012735f15b
saurabhgandhiwce <saurabhgandhi93@gmail.com>
2015-06-30 08:35:17 -0400

    Some change

--- feeds/views.py
+++ feeds/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from pymongo.mongo_client import MongoClient
 from rest_framework.response import Response
 from feeds.basic_authentication import basic_authentication
-from PIL import Image   
 class feed(APIView):
 
     def get(self, request):
@@ -12,8 +11,8 @@ class feed(APIView):
             page =int(request.query_params.get('page',1))
             client = MongoClient()
             db = client.Explore
-            collection = db.Story
-            response = [post for post in collection.find({}, {'_id': 0}).skip((page -1)*5).limit(5)]
+            collection = db.story
+            response = [post for post in collection.find({}, {'_id': 0}).skip((page -1)*5).limit(5).sort('index',direction=1)]
             return Response(response)
         
         return Response({"Not Authenticated":304})
